b9d3f73bad7131fc38c12f789730a353f0513e79
Filipe Covre <filipecovre224@gmail.com>
2024-10-29 17:51:10 -0300

    Implementando layout do chat

--- resources/js/components/chats/allChats.vue
+++ resources/js/components/chats/allChats.vue
@@ -1,6 +1,6 @@
 <template>
     <li class="nav-item w-100">
-        <GoTo to="" customClass="nav-link align-middle px-2 chat-item">
+        <GoTo to="/chat" customClass="nav-link align-middle px-2 chat-item">
             <i class="fa-regular fa-comment"></i>
             <span class="d-none d-sm-inline ml-2">Chat 1</span>
         </GoTo>
